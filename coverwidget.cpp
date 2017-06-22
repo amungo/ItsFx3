3daf3576d7463c6a89514c87fcfb8526954dd1a3
Viktor Klimenko <shurup239@mail.ru>
2017-06-22 17:41:24 +0300

    painting example

--- coverwidget.cpp
+++ coverwidget.cpp
@@ -22,5 +22,18 @@ void CoverWidget::paintEvent(QPaintEvent *)
     painter.drawLine( W2, 0, W2, H );
 
     painter.drawLine( 0, H2, W, H2 );
+
+    QPoint center( W2, H2 );
+    QColor color( Qt::red );
+    painter.setPen( QPen( color, 4, Qt::SolidLine) );
+    painter.setBrush( QBrush( color ) );
+    painter.drawEllipse( center, 4, 4 );
+
+    float perc = 0.05;
+    int rad = H*perc;
+    color = QColor(128, 128, 255, 32);
+    painter.setPen( QPen( color, 1, Qt::SolidLine) );
+    painter.setBrush( QBrush(color) );
+    painter.drawEllipse( center, rad, rad );
 }
 
