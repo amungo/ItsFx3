9fae9005198ef65e52b1f87c481e489c286b6420
mivcamry <mivcamry@gmail.com>
2018-05-11 17:47:33 +0300

    Renamed methods for Lattice Control

--- hwfx3/FX3Dev.h
+++ hwfx3/FX3Dev.h
@@ -56,19 +56,20 @@ public:
     fx3_dev_debug_info_t getDebugInfoFromBoard( bool ask_speed_only = false );
 
     //------------------------  Lattice (FX3DevIfce interFACE)  -------------------
-    virtual fx3_dev_err_t send16bitSPI_ECP5(uint8_t _data, uint8_t _addr);
-    virtual fx3_dev_err_t sendECP5(uint8_t* _data, long _data_len);
-    virtual fx3_dev_err_t recvECP5(uint8_t* _buf, long _len);
+    virtual fx3_dev_err_t send8bitSPI(uint8_t addr, uint8_t data);
+    virtual fx3_dev_err_t read8bitSPI(uint8_t addr, uint8_t* data);
+    virtual fx3_dev_err_t sendECP5(uint8_t* buf, long len);
+    virtual fx3_dev_err_t recvECP5(uint8_t* buf, long len);
     virtual fx3_dev_err_t resetECP5();
     virtual fx3_dev_err_t checkECP5();
     virtual fx3_dev_err_t csonECP5();
     virtual fx3_dev_err_t csoffECP5();
-    virtual fx3_dev_err_t send24bitSPI8bit(unsigned int _data);
+    virtual fx3_dev_err_t setDAC(unsigned int data);
     virtual fx3_dev_err_t device_start();
     virtual fx3_dev_err_t device_stop();
     virtual fx3_dev_err_t device_reset();
     virtual fx3_dev_err_t reset_nt1065();
-    virtual fx3_dev_err_t load1065Ctrlfile(const char* _fwFileName, int _lastaddr);
+    virtual fx3_dev_err_t load1065Ctrlfile(const char* fwFileName, int lastaddr);
 
     // ****
 protected:
